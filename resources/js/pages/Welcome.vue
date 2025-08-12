3cd8bafae0c064969114d9cbc3acf2a6307dac8c
Liam <liam3006x@gmail.com>
2025-08-13 00:35:23 +0100

    Add inertiajs/vue/ts

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+    </Head>
+    <div class="bg-black flex h-screen items-center justify-center text-white w-full">
+        <h1
+            class="
+                font-bold font-serif text-2xl tracking-widest underline underline-offset-8
+                lg:text-5xl
+            "
+        >
+            LXST.ADMIN
+        </h1>
+    </div>
+</template>
