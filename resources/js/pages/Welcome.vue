af8c6888648acf08f3fd4ac23821d4fcd323dc52
Liam <liam3006x@gmail.com>
2025-08-13 00:41:18 +0100

    Add eslint/prettier

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Welcome">
         <link rel="preconnect" href="https://rsms.me/" />
     </Head>
-    <div class="bg-black flex h-screen items-center justify-center text-white w-full">
-        <h1
-            class="
-                font-bold font-serif text-2xl tracking-widest underline underline-offset-8
-                lg:text-5xl
-            "
-        >
-            LXST.ADMIN
-        </h1>
+    <div class="flex h-screen w-full items-center justify-center bg-black text-white">
+        <h1 class="font-serif text-2xl font-bold tracking-widest underline underline-offset-8 lg:text-5xl">LXST.ADMIN</h1>
     </div>
 </template>
