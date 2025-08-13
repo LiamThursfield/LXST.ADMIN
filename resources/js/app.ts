1f3b3a6b527d7448aaea4f549d882f4ff61a8617
Liam <liam3006x@gmail.com>
2025-08-13 22:29:57 +0100

    Add PrimeVue

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,9 +3,31 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
+import PrimeVue from 'primevue/config';
+import { definePreset } from '@primeuix/themes';
+
+import Aura from '@primeuix/themes/aura';
 import { createApp, h } from 'vue';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'LXST.ADMIN';
+
+const LxstAdminPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
@@ -13,6 +35,11 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: LxstAdminPreset
+                }
+            })
             .mount(el);
     },
     progress: {
--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
+import ProgressBar from "primevue/progressbar"
 </script>
 
 <template>
@@ -7,6 +8,10 @@ import { Head } from '@inertiajs/vue3';
         <link rel="preconnect" href="https://rsms.me/" />
     </Head>
     <div class="flex h-screen w-full items-center justify-center bg-black text-white">
-        <h1 class="font-serif text-2xl font-bold tracking-widest underline underline-offset-8 lg:text-5xl">LXST.ADMIN</h1>
+        <div>
+            <h1 class="font-serif text-2xl font-bold tracking-widest lg:text-5xl">LXST.ADMIN</h1>
+            <ProgressBar mode="indeterminate" class="mt-1 !h-2"></ProgressBar>
+        </div>
+
     </div>
 </template>
