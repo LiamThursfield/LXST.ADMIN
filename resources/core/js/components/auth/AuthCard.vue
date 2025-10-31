16358df2ec1908f327b4e1843dba3cefdf2e2ae2
Liam <liam3006x@gmail.com>
2025-10-31 19:27:55 +0000

    Implement Auth Card and layout

---
+++ resources/core/js/components/auth/AuthCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col max-w-md space-y-3 w-full">
+        <Card>
+            <template #title>
+                <h1 class="text-center">{{ title }}</h1>
+            </template>
+
+            <template #content>
+                <div class="mt-4">
+                    <slot />
+                </div>
+            </template>
+        </Card>
+
+        <div class="px-3 text-right text-white">
+            <slot name="footer"/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {Card} from "primevue";
+
+defineProps<{
+    title: string
+}>();
+</script>
